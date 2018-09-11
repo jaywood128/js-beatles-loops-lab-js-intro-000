0685b55ab5525ba172b27443a05e016eadedfaf4
Johnathon Wood <jaywood128@gmail.com>
2018-09-11 18:31:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []; 
+
+  for ( let i = 0; i = musicians.length; i++) {
+    arr.push(${musicans}) + arr.push(${instruments})
+
+    return arr
+} 
