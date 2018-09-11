681743750bde28d6bcac204ac785dfd5ed361f4a
Johnathon Wood <jaywood128@gmail.com>
2018-09-11 18:42:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   const arr = []
 
-  for ( let i = 0; 1 = musicians.length; i < 1; i++) {
+  for ( let i = 0, 1 = musicians.length; i < 1; i++) {
     arr.push(`${musicans[i]} plays ${instruments[i]}`)
   }
     return arr
