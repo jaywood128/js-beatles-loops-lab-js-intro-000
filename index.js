6da65ecc11a9a2b61e45333fc35b3617f3ebd0f8
Johnathon Wood <jaywood128@gmail.com>
2018-09-12 13:56:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   const arr = []
 
   for (let i = 0, l = musicians.length; i < l; i++) {
-    
+
     arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 
@@ -11,11 +11,11 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(array) {
   const facts = []
 
-  let i = 0 
-  while (i < length.facts; i++) {
+  let i = 0
+  while (i < length.facts, i++) {
    facts.push(`${facts[i]}!!!`)
      return facts
   }
 
   return facts
-} 
+}
